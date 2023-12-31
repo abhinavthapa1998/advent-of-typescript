2c9c23f696fac1122189449d2291ff44886473da
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-12-31 18:17:36 +0530

    day-20 solution added

---
+++ day-20/day-20-solution.ts
@@ -0,0 +1,32 @@
+type Letters = {
+  A: ["█▀█ ", "█▀█ ", "▀ ▀ "];
+  B: ["█▀▄ ", "█▀▄ ", "▀▀  "];
+  C: ["█▀▀ ", "█ ░░", "▀▀▀ "];
+  E: ["█▀▀ ", "█▀▀ ", "▀▀▀ "];
+  H: ["█ █ ", "█▀█ ", "▀ ▀ "];
+  I: ["█ ", "█ ", "▀ "];
+  M: ["█▄░▄█ ", "█ ▀ █ ", "▀ ░░▀ "];
+  N: ["█▄░█ ", "█ ▀█ ", "▀ ░▀ "];
+  P: ["█▀█ ", "█▀▀ ", "▀ ░░"];
+  R: ["█▀█ ", "██▀ ", "▀ ▀ "];
+  S: ["█▀▀ ", "▀▀█ ", "▀▀▀ "];
+  T: ["▀█▀ ", "░█ ░", "░▀ ░"];
+  Y: ["█ █ ", "▀█▀ ", "░▀ ░"];
+  W: ["█ ░░█ ", "█▄▀▄█ ", "▀ ░ ▀ "];
+  " ": ["░", "░", "░"];
+  ":": ["#", "░", "#"];
+  "*": ["░", "#", "░"];
+};
+
+type RowPrinter<
+  T extends string,
+  N extends number
+> = Uppercase<T> extends `${infer F extends keyof Letters}${infer R}`
+  ? `${Letters[Uppercase<F>][N]}${RowPrinter<R, N>}`
+  : "";
+
+type ToAsciiArt<T extends string> = T extends `${infer F}\n${infer R}`
+  ? [...ToAsciiArt<F>, ...ToAsciiArt<R>]
+  : T extends string
+  ? [RowPrinter<T, 0>, RowPrinter<T, 1>, RowPrinter<T, 2>]
+  : [];
